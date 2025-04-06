5464d35d6574c281417d9efea359a4f77eb7637a
He核 <72736679+hzy-dog@users.noreply.github.com>
2025-04-07 01:37:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ const MAX_HISTORY_ITEMS = 5;
 // 网站信息配置
 const SITE_CONFIG = {
     name: 'D-TV',
-    url: 'https://d-tv.hzy0438561.top',
+    url: 'https://d-tv.hzy40214.top',
     description: '免费在线视频搜索与观看平台',
     logo: 'https://images.icon-icons.com/38/PNG/512/retrotv_5520.png',
     version: '1.0.0'
